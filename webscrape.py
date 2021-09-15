504702028cdea2c23de15748a3adc02424f871d8
golem019 <golem019@umn.edu>
2021-09-14 19:06:10 -0500

    added modularity for search terms and finished exporting to various files

--- webscrape.py
+++ webscrape.py
@@ -2,18 +2,21 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
-def extract(page):
+search_terms = [f'python%20developer', f'java%20developer', f'C%2B%2B%20Developer']
+job_terms = ['Python Developer', 'Java Developer', 'C++ Developer']
+
+def extract(page, term):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'}
-    url = f'https://www.indeed.com/jobs?q=python%20developer&l=Minnesota&start={page}'
+    url = f'https://www.indeed.com/jobs?q={term}&l=Minnesota&start={page}'
     r = requests.get(url, headers)
     soup = BeautifulSoup(r.content, 'lxml')
     return soup
 
 
 def transform(soup):
-    divs = soup.find_all('div', class_='slider_container')
-    for item in divs:
+    listings = soup.find_all('div', class_='slider_container')
+    for item in listings:
         job_title = item.find('h2').find(class_=None).text
         company = item.find('span', class_='companyName').text
         try:
@@ -31,16 +34,46 @@ def transform(soup):
         joblist.append(job)
     return
 
-joblist=[]
-for page in range(0, 40, 10):
-    print(f'Getting data on page: {page//10}')
-    c = extract(page)
-    transform(c)
-print(len(joblist))
+def get_salary(listing):
+    temp = listing['salary'].replace(',', '').replace('$', '') #gets rid of extra symbols in numbers
+    salary_range = [int(i) for i in temp.split() if i.isdigit()]
+    final_salary = 0
+    for num in salary_range:
+        final_salary += num
+    if len(salary_range) != 0:
+        return final_salary // len(salary_range)
+    return 0
 
 
-job_listings_csv = pd.DataFrame(joblist)
-job_listings_csv.to_csv('jobs.csv')
+def find_avg_salary():
+    avg_salary = num_salary = 0
+    for listing in joblist:
+        if listing['salary'] != '':
+            salary = get_salary(listing)
+            avg_salary += salary
+            num_salary = num_salary + 1
+            if salary < 1000:
+                salary *= 2000 # turn hourly wage into yearly wage
+    avg_salary_job = {
+        'title': 'Average Salary',
+        'company': '',
+        'salary': round(avg_salary//num_salary, -3),
+        'summary': '' 
+    }
+    return avg_salary_job
 
-job_listings_excel = pd.DataFrame(joblist)
-job_listings_excel.to_excel('jobs.xlsx')
+for i in range (len(search_terms)):
+    joblist = []
+
+    # get first 5 pages of listings
+    for page in range(0, 40, 10):
+        print(f'Getting data on page: {page//10}')
+        c = extract(page, search_terms[i])
+        transform(c)
+    joblist.append(find_avg_salary())
+
+    job_listings_csv = pd.DataFrame(joblist)
+    job_listings_csv.to_csv(f'{job_terms[i]}.csv')
+
+    job_listings_excel = pd.DataFrame(joblist)
+    job_listings_excel.to_excel(f'{job_terms[i]}.xlsx')
